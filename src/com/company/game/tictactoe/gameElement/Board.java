8a9c6818ff296664ec22c4a1e7dcb43208fde399
dwaffe <dwaffe@gmail.com>
2019-09-07 14:49:31 +0200

    work in progress

---
+++ src/com/company/game/tictactoe/gameElement/Board.java
@@ -0,0 +1,13 @@
+package com.company.game.tictactoe.gameElement;
+
+public class Board {
+    Piece[][] board;
+
+    public Board(int xLength, int yLength) {
+        board = new Piece[xLength][yLength];
+    }
+
+    public void put(int makeMove, Piece piece) {
+
+    }
+}
